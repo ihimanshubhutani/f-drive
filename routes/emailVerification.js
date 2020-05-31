9a7733ce04fb2dc502574a098d92127fe1fd6568
ihimanshubhutani <bhutanihimanshu98@gmail.com>
2020-05-31 16:58:35 +0530

    feat: added email sending service

--- routes/emailVerification.js
+++ routes/emailVerification.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const path = require("path");
-
+const config = require("config");
 const routes = express.Router();
 const keys = require("../config/keys.json");
 const { insertVerificationCode } = require("../controller/emailVerification");
@@ -8,6 +7,7 @@ const uuid = require("uuid");
 
 routes.get("/verify", (req, res) => {
   const code = uuid.v4();
+
   insertVerificationCode(req.body.email, code);
   const send = require("gmail-send")({
     user: keys.GOOGLE.USERNAME,
@@ -18,7 +18,8 @@ routes.get("/verify", (req, res) => {
 
   send(
     {
-      text: `Thank You for registering with Us \n Your verification code is ${code}`,
+      text: `Thank You for registering with Us \n Click below to confim your email address \n
+       http://${config.SERVER}/email/verification-service?validemail=${req.body.email}&code=${code}&`,
     },
     (error, result, fullResult) => {
       if (error) console.error(error);
