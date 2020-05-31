c72c4f64ac001a3afbefc3c7812eef620a0ebda7
ihimanshubhutani <bhutanihimanshu98@gmail.com>
2020-05-31 17:19:55 +0530

    style: changed name of emailVerification

--- routes/emailVerification.js
+++ routes/emailVerification.js
@@ -3,29 +3,16 @@ const config = require("config");
 const routes = express.Router();
 const keys = require("../config/keys.json");
 const { insertVerificationCode } = require("../controller/emailVerification");
+const {
+  sendEmailForVerification,
+} = require("../controller/sendEmailForVerification");
+
 const uuid = require("uuid");
 
 routes.get("/verify", (req, res) => {
-  const code = uuid.v4();
-
-  insertVerificationCode(req.body.email, code);
-  const send = require("gmail-send")({
-    user: keys.GOOGLE.USERNAME,
-    pass: keys.GOOGLE.PASS,
-    to: req.query.validemail,
-    subject: "Verification Email -- Fdrive",
+  insertVerificationCode(req.query.email, req.query.code).then(() => {
+    sendEmailForVerification(email);
   });
-
-  send(
-    {
-      text: `Thank You for registering with Us \n Click below to confim your email address \n
-       http://${config.SERVER}/email/verification-service?validemail=${req.body.email}&code=${code}&`,
-    },
-    (error, result, fullResult) => {
-      if (error) console.error(error);
-      console.log(result, fullResult);
-    }
-  );
 });
 
 module.exports = routes;
