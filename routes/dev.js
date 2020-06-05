1e6b49a64d0a651d0c75a03a74b9c6f841238c50
=Himansu Bhutani <bhutanihimanshu98@gmail.com>
2020-06-05 15:41:36 +0530

    feat: added developer signup and login page and dev routes

--- routes/dev.js
+++ routes/dev.js
@@ -2,9 +2,22 @@ const express = require("express");
 const config = require("config");
 const routes = express.Router();
 const path = require("path")
-routes.get('/', (req, res) => {
 
+routes.get('/', (req, res) => {
     res.sendFile("devLogin.html", { root: path.join(__dirname, "../views/") });
-})
+});
+
+routes.get('/signup', (req, res) => {
+    res.sendFile("devSignup.html", { root: path.join(__dirname, "../views/") });
+});
+
+routes.post('/login', (req, res) => {
+
+});
+
+routes.post('/signup', (req, res) => {
+
+});
+
 
 module.exports = routes;
