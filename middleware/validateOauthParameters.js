816253eea01c5f8432a093107d1aba114dca58f9
=Himansu Bhutani <bhutanihimanshu98@gmail.com>
2020-06-06 21:02:45 +0530

    refactor: added invalid parameters message

--- middleware/validateOauthParameters.js
+++ middleware/validateOauthParameters.js
@@ -1,3 +1,5 @@
+const config = require('../config/default.json');
+
 module.exports = (req, res, next) => {
     const query = req.query;
     const scope = query.scope;
@@ -6,5 +8,14 @@ module.exports = (req, res, next) => {
     const accessType = query.access_type;
     const clientId = query.client_id;
     const state = query.state;
+
+    if (!scope) return res.status(400).send({ message: config.MISSING_PARAMS.SCOPE });
+    if (!responseType) return res.status(400).send({ message: config.MISSING_PARAMS.RESPONSE_TYPE });
+    if (!redirectUri) return res.status(400).send({ message: config.MISSING_PARAMS.REDIRECT_URI });
+    if (!accessType) return res.status(400).send({ message: config.MISSING_PARAMS.ACCESS_TYPE });
+    if (!clientId) return res.status(400).send({ message: config.MISSING_PARAMS.CLIENT_ID });
+    if (!state) return res.status(400).send({ message: config.MISSING_PARAMS.STATE });
+
+
     next();
 }
--- routes/oauth.js
+++ routes/oauth.js
@@ -2,19 +2,9 @@ const express = require("express");
 const config = require("config");
 const routes = express.Router();
 const path = require("path");
+const validateOauthParameters = require("../middleware/validateOauthParameters");
 
-routes.get('/', (req, res) => {
-
-    const query = req.query;
-    const scope = query.scope;
-    const responseType = query.response_type;
-    const redirectUri = query.redirect_uri;
-    const accessType = query.access_type;
-    const clientId = query.client_id;
-    const state = query.state;
-    console.log(scope, responseType, redirectUri, accessType, clientId, state);
-    if (!(scope & responseType & redirectUri & accessType & clientId & state)) return res.status(400).send({ message: 'config.MESSAGE.MISSING_PARAMETERS' });
-
+routes.get('/', validateOauthParameters, (req, res) => {
 
     res.render(path.join(__dirname, "../views/oauthLogin"), {});
 
