aa0215d07d20f320072150782e489d4644bfab33
=Himansu Bhutani <bhutanihimanshu98@gmail.com>
2020-06-11 12:13:59 +0530

    integerated file controller

--- routes/files.js
+++ routes/files.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { unlinkSync } from 'fs';
-import { join, extname } from 'path';
-import { MESSAGE } from 'config';
 
+import {
+  showUploadPage, downloadFile, showFiles, saveFile, deleteFile,
+} from '../controller/fileController';
 import authenticate from '../middleware/authenticateSession';
 import checkAccessAllowed from '../middleware/checkAccesPrivilages';
-import uploadFile from '../util/fileUploader';
 
-import { deleteFilePath, showUserFiles } from '../services/file/filesDataHandler';
 
 const routes = Router();
 
@@ -16,46 +14,10 @@ const routes = Router();
  */
 routes.use(authenticate);
 
-routes.get('/', (req, res, next) => {
-  showUserFiles(req.session.userId, res)
-    .then(result => {
-      const data = JSON.stringify({ arr: result });
-      res.render(join(__dirname, '../views/viewFiles'), { data });
-    })
-    .catch(err => next(err));
-});
-
-routes.get('/upload', (req, res) => {
-  res.sendFile('upload.html', { root: join(__dirname, '../views/') });
-});
-
-routes.get('/:id', checkAccessAllowed, (req, res) => {
-  res.download(req.filepath, req.filepath.split('*')[1]);
-});
-
-routes.post('/', (req, res) => {
-  if (!req.files) { res.status(400); throw new Error(MESSAGE.NO_FILE_UPLOADED); }
-
-  const file = req.files.uploadedFile;
-
-  if (uploadFile(file, file.name, req.session.userId, extname(file.name), Date.now())) {
-    return res.status(201).send({
-      message: MESSAGE.FILE_UPLOADED_SUCCESS,
-    });
-  }
-
-  throw new Error(MESSAGE.INTERNAL_SERVER_ERROR);
-});
-
-
-routes.delete('/:id', checkAccessAllowed, (req, res, next) => {
-  deleteFilePath(req.params.id).then((result) => {
-    if (!result) throw new Error(MESSAGE.INTERNAL_SERVER_ERROR);
-
-    unlinkSync(req.filepath);
-    res.json({ message: MESSAGE.REMOVED_SUCCESSFULLY });
-  }).catch(err => next(err));
-});
-
+routes.get('/', showFiles);
+routes.get('/upload', showUploadPage);
+routes.get('/:id', checkAccessAllowed, downloadFile);
+routes.post('/', saveFile);
+routes.delete('/:id', checkAccessAllowed, deleteFile);
 
 export default routes;
