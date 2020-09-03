f800b99f58379fea391803c945da6cc7f8d86dd9
ihimanshubhutani <bhutanihimanshu98@gmail.com>
2020-09-03 14:58:36 +0530

    updated: routes

--- app/routes/f-drive.js
+++ app/routes/f-drive.js
@@ -4,14 +4,9 @@ import {
   returnFilesData, returnFileMetaData, returnProfileData, downloadFileWithAccessToken,
 } from '../controller/fDriveController';
 
-
 const routes = Router();
-routes.get('/okok', (req, res) => {
-  console.log('called');
-  res.download('./public/1/1591860558238*Himanshu Bhutani Resume.pdf');
-});
-routes.use(verifyAccessToken);
 
+routes.use(verifyAccessToken);
 routes.get('/file', returnFilesData);
 routes.get('/file/:id', returnFileMetaData);
 routes.get('/profile', returnProfileData);
